7fc8e61152283a2368345a5623b5aa1fbc848c33
animesh65432 <kiranduttta234@gmail.com>
2024-02-13 14:59:43 +0530

    make log in page

--- src/Components/Login/Log.jsx
+++ src/Components/Login/Log.jsx
@@ -1,11 +1,15 @@
 import React, { useState } from "react";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword
+} from "firebase/auth";
 import { auth } from "../../firbase";
 
 const Log = () => {
   const [userEmail, setUserEmail] = useState("");
   const [userPassword, setUserPassword] = useState("");
   const [isloading, setIsLoading] = useState(false);
+  const [IsSingin, Setsingin] = useState(false);
 
   const onchangeUserEmail = (e) => {
     setUserEmail(e.target.value);
@@ -15,64 +19,86 @@ const Log = () => {
     setUserPassword(e.target.value);
   };
 
+  const toggole = () => {
+    Setsingin((prev) => !prev);
+  };
+
   const onSubmitHandler = async (e) => {
     e.preventDefault();
     setIsLoading(true);
 
-    try {
-      const response = await createUserWithEmailAndPassword(
-        auth,
-        userEmail,
-        userPassword
-      );
-      console.log(response);
-    } catch (error) {
-      let errorMessage = error.message; // Accessing the error message
-      alert(errorMessage);
+    if (IsSingin) {
+      try {
+        let reponse = await signInWithEmailAndPassword(
+          auth,
+          userEmail,
+          userPassword
+        );
+        console.log(reponse);
+      } catch (error) {
+        console.assert(error);
+      }
+    } else {
+      try {
+        const response = await createUserWithEmailAndPassword(
+          auth,
+          userEmail,
+          userPassword
+        );
+        console.log(response);
+      } catch (error) {
+        let errorMessage = error.message;
+        alert(errorMessage);
+      }
     }
     setIsLoading(false);
   };
 
   return (
-    <form className="bg-white p-8 rounded-lg shadow-md">
-      <div className="mb-4">
-        <label
-          htmlFor="email"
-          className="block text-gray-700 text-sm font-bold mb-2"
+    <>
+      <div className="bg-white p-8 rounded-lg shadow-md h-4/5">
+        <div className="mb-4">
+          <label
+            htmlFor="email"
+            className="block text-gray-700 text-sm font-bold mb-2"
+          >
+            Email:
+          </label>
+          <input
+            id="email"
+            type="email"
+            placeholder="Please enter your email"
+            onChange={onchangeUserEmail}
+            className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:border-blue-500"
+          />
+        </div>
+        <div className="mb-6">
+          <label
+            htmlFor="password"
+            className="block text-gray-700 text-sm font-bold mb-2"
+          >
+            Password:
+          </label>
+          <input
+            id="password"
+            type="password"
+            placeholder="Please enter your password"
+            onChange={onchangePassword}
+            className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:border-blue-500"
+          />
+        </div>
+        <button
+          type="submit"
+          onClick={onSubmitHandler}
+          className="w-full bg-blue-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
         >
-          Email:
-        </label>
-        <input
-          id="email"
-          type="email"
-          placeholder="Please enter your email"
-          onChange={onchangeUserEmail}
-          className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:border-blue-500"
-        />
+          {IsSingin ? "log in" : "sign in"}
+        </button>
+        <button onClick={toggole}>
+          {!IsSingin ? "Create New Acoount" : "Continue With Your Account"}
+        </button>
       </div>
-      <div className="mb-6">
-        <label
-          htmlFor="password"
-          className="block text-gray-700 text-sm font-bold mb-2"
-        >
-          Password:
-        </label>
-        <input
-          id="password"
-          type="password"
-          placeholder="Please enter your password"
-          onChange={onchangePassword}
-          className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:border-blue-500"
-        />
-      </div>
-      <button
-        type="submit"
-        onClick={onSubmitHandler}
-        className="w-full bg-blue-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-      >
-        {isloading ? "Loading..." : "Log in"}
-      </button>
-    </form>
+    </>
   );
 };
 
